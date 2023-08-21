17fee0aaf5ce5111f79057f0957a71b9dc4cbcd8
gabrielemafrica <gabriele.ga@live.it>
2023-08-21 18:01:25 +0200

    bonus 1

--- vite-boolfolio/src/components/Projects.vue
+++ vite-boolfolio/src/components/Projects.vue
@@ -9,27 +9,30 @@ export default {
     },
     data: function() {
         return {
-          projects:[]
+          projects:[],
+          pages: []
         };
     },
   methods: {
-    
-  },
-  mounted() {
-    axios.get('http://127.0.0.1:8000/api/v1/projects')
-      .then (response => {
+    callProjects(urlApi){
+      axios.get(urlApi)
+      .then(response=>{
         const data = response.data;
         console.log(data);
 
-        this.projects = data.projects;
-        console.log('ecco cosa esce');
-        console.log(this.projects);
-        console.log('ecco cosa esce 2');
-        console.log(this.projects[0].nome);
-      })
-      .catch(error => {
+        this.projects = data.projects.data;
+        this.pages = data.projects.links;
+        })
+      .catch(error=>{
         console.log(error);
       })
+    },
+    loadPage(url){
+      this.callProjects(url);
+    }
+  },
+  mounted() {
+    this.callProjects('http://127.0.0.1:8000/api/v1/projects');
   }
 };
   
@@ -37,15 +40,33 @@ export default {
 
 <template>
   <div class="container text-center my-5">
-  <h1>Progetti</h1>
+    <h1>Progetti</h1>
 
-  <div class="container d-flex justify-content-center gap-2">
+    <div class="container d-flex justify-content-center gap-2 my-4">
     
-    <div v-for="project in projects" :key="project.id">
-      <SingleProject :projectObj="project" />
-    </div>
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :projectObj="project" />
+      </div>
   
-  </div>
+    </div>
+
+    <div class="container d-flex justify-content-center gap-2">
+      <div v-for="(page, index) in pages" :key="index">
+        <!-- class con sintassi ad oggetto -->
+        <button
+          :class="{
+            'btn': true,
+            'btn-warning': page.active,
+            'btn-primary': !page.active,
+            'disabled': page.url == null,
+          }"
+          v-html="page.label"
+          @click="loadPage(page.url)"
+          >
+        </button>
+      </div>
+    </div>
+
   </div>
 
   
