c4fca5a109ce0dda5af3365ac93b09cbfb137380
gabrielemafrica <gabriele.ga@live.it>
2023-08-21 17:03:02 +0200

    chiamata axios e show

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Projects from './components/Projects.vue';
+
+export default {
+  components: {
+    Projects
+  },
+}
+</script>
+
+<template>
+  <Projects />
+</template>
+
+<style lang="scss">
+
+@use './styles/general.scss' as *;
+
+</style>
---
+++ vite-boolfolio/src/components/Projects.vue
@@ -0,0 +1,53 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Projects',
+    data: function() {
+        return {
+          projects:[]
+        };
+    },
+  methods: {
+    
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/v1/projects')
+      .then (response => {
+        const data = response.data;
+        console.log(data);
+
+        this.projects = data.projects;
+        console.log('ecco cosa esce');
+        console.log(this.projects);
+        console.log('ecco cosa esce 2');
+        console.log(this.projects[0].nome);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+};
+  
+</script>
+
+<template>
+  <div class="container text-center">
+  <h1>hello</h1>
+
+  <div class="container">
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.nome }}
+      </li>
+    </ul>
+  </div>
+  </div>
+
+  
+</template>
+
+<style  lang="scss">
+
+@use '../styles/general.scss' as *;
+</style>
